2a8b6d61f77feaddf6b29175fbc1fce55cef9683
Muhammad Usman Baber <112895478+UsmanKamboh0@users.noreply.github.com>
2023-05-01 11:39:39 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,14 @@
 const mongoose=require('mongoose');
-const connection=()=>{
-    mongoose.connect(process.env.DB_UI,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
+const PORT = process.env.PORT || 4000
 
-        console.log(`mongoose connected with server:${data.connection.host}`)
-    }) 
+const connection = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb+srv://usmanshop:Usman12345@easyshopping.x8xdn9x.mongodb.net/UsmanShop?retryWrites=true&w=majority');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
+
 module.exports=connection;
